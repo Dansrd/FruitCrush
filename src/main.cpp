ad994a42a99b473d9cee6d348a12ee84d6d2b603
Dansrd <daniel_rd@live.com>
2021-12-01 09:04:05 -0300

    Estrutura princial

---
+++ src/main.cpp
@@ -0,0 +1,35 @@
+#include "PIG.h"
+
+PIG_Evento evento;          //evento ser tratado a cada passada do loop principal
+PIG_Teclado meuTeclado;     //vari�vel como mapeamento do teclado
+
+int main( int argc, char* args[] ){
+
+    //criando o jogo (aplica��o)
+    CriaJogo("Meu Jogo");
+
+    //associando o teclado (basta uma �nica vez) com a vari�vel meuTeclado
+    meuTeclado = GetTeclado();
+
+    //loop principal do jogo
+    while(JogoRodando()){
+
+        //pega um evento que tenha ocorrido desde a �ltima passada do loop
+        evento = GetEvento();
+
+        //aqui o evento deve ser tratado e tudo deve ser atualizado
+
+        //ser� feita a prepara��o do frame que ser� exibido na tela
+        IniciaDesenho();
+
+        //todas as chamadas de desenho devem ser feitas aqui na ordem desejada
+
+        //o frame totalmente pronto ser� mostrado na tela
+        EncerraDesenho();
+    }
+
+    //o jogo ser� encerrado
+    FinalizaJogo();
+
+    return 0;
+}
